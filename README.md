529bf120531db3128ac382d424c61f85ee42c8e1
TimMikeladze <tim.mikeladze@gmail.com>
2023-08-09 01:14:33 -0700

    feat: kick off the project

---
+++ README.md
@@ -0,0 +1,21 @@
+# 🛠️ next-tool
+
+A tool to build more tools.
+
+> 🚧 Under active development. Expect breaking changes until v1.0.0.
+
+## 📡 Install
+
+```console
+npm install next-tool
+
+yarn add next-tool
+
+pnpm add next-tool
+```
+
+> 👋 Hello there! Follow me [@linesofcode](https://twitter.com/linesofcode) or visit [linesofcode.dev](https://linesofcode.dev) for more cool projects like this one.
+
+## 🚀 Getting Started
+
+Checkout an existing package to see how it works: [next-invite](https://github.com/TimMikeladze/next-invite/tree/master/src/NextInvite.ts).
---
+++ src/index.ts
@@ -0,0 +1,162 @@
+import { NextRequest, NextResponse } from 'next/server.js';
+import { NextApiRequest, NextApiResponse } from 'next/types';
+
+export type NextToolRequest = {
+  body?: {
+    action: string;
+    input: any;
+  };
+  headers?: Headers;
+};
+
+export type NextToolSendFn = <JsonBody>(
+  body: JsonBody,
+  init?: { status?: number }
+) => NextResponse<JsonBody> | Promise<void>;
+
+export type NextToolHandlerArgs = {
+  request: NextToolRequest;
+  send: NextToolSendFn;
+};
+
+export type NextToolGetStoreFn<Store> = () => Promise<Store | undefined>;
+
+export type NextToolActionConfig = {
+  after?: (input: any, request?: NextToolRequest) => Promise<any>;
+  before?: (input: any, request?: NextToolRequest) => Promise<any>;
+  disabled?: boolean;
+};
+
+export type NextToolConfig = {
+  actions?: Record<string, NextToolActionConfig>;
+};
+
+export type NextToolActionFn<Input = any, Output = any> = (
+  input: Input,
+  request?: NextToolRequest
+) => Promise<Output>;
+
+export abstract class NextTool<Config extends NextToolConfig, Store> {
+  protected store: Store | undefined;
+
+  protected getStoreFn: NextToolGetStoreFn<Store>;
+
+  protected config: Config;
+
+  protected actionMap: Record<string, NextToolActionFn> = {};
+
+  constructor(
+    config: Config,
+    store: NextToolGetStoreFn<Store> | Store,
+    actionMap?: Record<string, NextToolActionFn>
+  ) {
+    this.config = config;
+    this.actionMap = actionMap || {};
+    const getStoreFn = (
+      store !== undefined
+        ? typeof store === 'function'
+          ? store
+          : () => Promise.resolve(store)
+        : undefined
+    ) as NextToolGetStoreFn<Store>;
+    if (!getStoreFn) {
+      throw new Error('store is undefined');
+    }
+    this.getStoreFn = getStoreFn;
+  }
+
+  public getConfig() {
+    return this.config;
+  }
+
+  public getStore() {
+    return this.store;
+  }
+
+  public async init() {
+    if (!this.store && this.getStoreFn) {
+      this.store = await this.getStoreFn?.();
+    }
+  }
+
+  public async handler(request: NextRequest) {
+    const body = await request.json();
+
+    return this.rawHandler({
+      send: NextResponse.json,
+      request: {
+        body,
+        headers: request.headers,
+      },
+    });
+  }
+
+  public async pagesApiHandler(
+    request: NextApiRequest,
+    response: NextApiResponse
+  ) {
+    const { body, headers } = request;
+
+    const json = async (data: any, options?: { status?: number }) =>
+      response.status(options?.status || 200).json(data);
+
+    return this.rawHandler({
+      send: json,
+      request: {
+        body,
+        headers: headers as any,
+      },
+    });
+  }
+
+  public async rawHandler(args: NextToolHandlerArgs) {
+    const { send, request } = args;
+
+    if (!request.body) {
+      return send({ error: `No body` }, { status: 400 });
+    }
+
+    const { action, input } = request.body;
+
+    if (!action) {
+      return send({ error: `No action` }, { status: 400 });
+    }
+
+    const actionFn = this.actionMap[action];
+
+    if (!actionFn) {
+      return send({ error: `Unknown action "${action}"` }, { status: 400 });
+    }
+
+    const enabledActions = this.config.actions;
+
+    if (!enabledActions) {
+      return send({ error: `Action "${action}" not enabled` }, { status: 400 });
+    }
+
+    const actionConfig = enabledActions[action];
+
+    if (!actionConfig || actionConfig.disabled) {
+      return send({ error: `Action "${action}" not enabled` }, { status: 400 });
+    }
+
+    await this.init();
+
+    try {
+      const before = actionConfig.before
+        ? await actionConfig.before(input, request)
+        : input;
+
+      const res = await actionFn(before, request);
+
+      const data = actionConfig.after
+        ? await actionConfig.after(res, request)
+        : res;
+
+      return send({ data });
+    } catch (error) {
+      console.error(error);
+      return send({ error: (error as any).message }, { status: 500 });
+    }
+  }
+}
